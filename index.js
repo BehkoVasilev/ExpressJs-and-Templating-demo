9050fb053c899a1f7527f100677cfd9b8d695350
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-21 20:22:19 +0200

    post and delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 5000;
 
 app.get('/', (req, res) => {
     res.send(`
-    <h1 style="color: pink">Hello May!</h1>
+    <h1 style="color: pink">Hello form ExpressJS!</h1>
     <button type="button"><a href="/cats">Cats!</a></button>
     <button type="button"><a href="/dats">Dogs!</a></button>
     `);
@@ -15,11 +15,19 @@ app.get('/cats', (req, res) => {
     res.send('This is cats page!')
 });
 
-app.get('dogs', (req,res) =>{
+app.get('/dogs', (req,res) =>{
     res.send('This is dogs page!')
-})
+});
+
+app.post('/cats', (req, res) => {
+    res.send('Cat is recivied!');
+});
+
+app.delete('/cats', (req,res) =>{
+    res.send('Cat is deleted!')
+});
 app.get('*', (req, res) => {
     res.send('404');
-})
+});
 
 app.listen(port, () => console.log("Server is runing on port 5000..."));
