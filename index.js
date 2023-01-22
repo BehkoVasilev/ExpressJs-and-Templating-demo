e0c88168dee5f57a7fabeb77af3c1393d678ed10
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-22 16:37:40 +0200

    add static method and templates with handlebars

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
+
 const loggerMiddleware = require('./loggerMiddleware');
 
 const app = express();
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+
 const port = 5000;
 
 const validateCatId = (req, res, next) => {
@@ -13,26 +18,47 @@ const validateCatId = (req, res, next) => {
     next();
 };
 
+//middleware for all endpoints
 app.use(loggerMiddleware);
 
+//Static files / static middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
+    res.render('home')
+});
+
+
+app.get('/old', (req, res) => {
     res.send(`
-    <h1 style="color: pink">Hello form ExpressJS!</h1>
-    <button type="button"><a href="/cats">Cats!</a></button>
-    <button type="button"><a href="/dogs">Dogs!</a></button>
+    <head>
+        <link rel="stylesheet" href="css/style.css"
+    </head>
+    <body>
+        <h1 style="color: pink">Hello form ExpressJS!</h1>
+        <button type="button"><a href="/cats">Cats!</a></button>
+        <button type="button"><a href="/dogs">Dogs!</a></button>
+    </body>
     `);
 });
 
 app.get('/cats', (req, res) => {
-    res.send('This is cats page!')
+    res.render('cats')
 });
 //Middleware
 app.get('/cats/:catId', validateCatId, (req, res) => {
-    res.send(`<h1>Individual Cat Page with ID: ${req.params.catId}</h1>`);
+    // res.send(`<h1>Individual Cat Page with ID: ${req.params.catId}</h1>`);
+    res.render('cat', { id: req.params.catId })
 });
 
 app.get('/dogs', (req, res) => {
-    res.send('This is dogs page!')
+    const dogs = [
+        { name: 'Fiona', breed: 'English bulldog', age: '7' },
+        { name: 'Rex', breed: 'German Shepherd Dog', age: '3' },
+        { name: 'Herra', breed: 'Corso', age: '4' },
+    ]
+
+    res.render('dogs', { dogs })
 });
 
 //download
@@ -43,7 +69,7 @@ app.get('/dogs/5', (req, res) => {
 });
 
 app.get('/dogs/:dogId', (req, res) => {
-    res.send(`<h1>Individual Dog Page with ID: ${req.params.dogId}</h1>`)
+    res.send(` with ID: ${req.params.dogId}</h1>`)
 });
 
 app.post('/cats', (req, res) => {
