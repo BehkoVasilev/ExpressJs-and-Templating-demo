c57f16e014de4239befc282c2d05bcabee9fafb3
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-22 14:56:29 +0200

    a bit of middleware

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require('express');
+const loggerMiddleware = require('./loggerMiddleware');
 
 const app = express();
 const port = 5000;
+
+const validateCatId = (req, res, next) => {
+    let id = req.params.catId;
+
+    if (!Number(id)) {
+        return res.send('Invalid ID');
+    }
+    next();
+};
+
+app.use(loggerMiddleware);
 
 app.get('/', (req, res) => {
     res.send(`
     <h1 style="color: pink">Hello form ExpressJS!</h1>
     <button type="button"><a href="/cats">Cats!</a></button>
-    <button type="button"><a href="/dats">Dogs!</a></button>
+    <button type="button"><a href="/dogs">Dogs!</a></button>
     `);
 });
 
 app.get('/cats', (req, res) => {
     res.send('This is cats page!')
 });
-
-app.get('/cats/:catId', (req, res) => {
-    let id = req.params.catId;
-    console.log(req.params)
-    if (!Number(id)) {
-        res.send('404')
-    }
-    else {
-        res.send(`<h1>Individual Cat Page with ID: ${req.params.catId}</h1>`)
-    }
+//Middleware
+app.get('/cats/:catId', validateCatId, (req, res) => {
+    res.send(`<h1>Individual Cat Page with ID: ${req.params.catId}</h1>`);
 });
 
 app.get('/dogs', (req, res) => {
     res.send('This is dogs page!')
 });
 
+//download
 app.get('/dogs/5', (req, res) => {
-    // res.download('./dog.jpg');
-    res.sendFile('./dog.jpg', { root: __dirname })
+    res.download('./dog.jpg');
+    // res.sendFile('./dog.jpg', { root: __dirname });
     // res.attachment('./dog.jpg');//there is no end()
 });
 
